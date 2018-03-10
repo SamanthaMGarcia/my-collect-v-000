bde042e9f481b6dfbd36699ad5431ebb97d6311d
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-10 23:11:20 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,9 +2,9 @@ def my_collect(collection)
  i = 0
  
   while i < collection.length
-  yield collection[i]
+  yield collection[i].upcase
     i = i + 1
   end
-  collection.upcase
+  collection
 end
 
